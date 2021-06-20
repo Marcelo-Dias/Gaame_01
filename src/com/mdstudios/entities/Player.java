14c8d35a9d2a631491a7176d786a1aedebbcb084
Marcelo-Dias <34683289+Marcelo-Dias@users.noreply.github.com>
2021-06-20 18:31:57 -0300

    Atualização camera e clamp

--- src/com/mdstudios/entities/Player.java
+++ src/com/mdstudios/entities/Player.java
@@ -4,6 +4,8 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 import com.mdstudios.main.Game;
+import com.mdstudios.world.Camera;
+import com.mdstudios.world.World;
 
 public class Player extends Entity{
 	
@@ -60,13 +62,17 @@ public class Player extends Entity{
 					index = 0;
 			}
 		}
+		
+		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH / 2), 0, World.WIDTH * 16 - Game.WIDTH);
+		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT / 2), 0, World.HEIGHT * 16 - Game.HEIGHT);
+		
 	}
 	
 	public void render(Graphics g) {
 		if(dir == right_dir) {
-			g.drawImage(rightPlayer[index], this.getX(), this.getY(), null);
+			g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
 		}else if(dir == left_dir) {
-			g.drawImage(leftPlayer[index], this.getX(), this.getY(), null);
+			g.drawImage(leftPlayer[index], this.getX()- Camera.x, this.getY() - Camera.y, null);
 		}
 	}
 
