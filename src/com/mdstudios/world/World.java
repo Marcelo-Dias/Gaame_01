0eb8b253b75742ac415c5595bc287c573900a813
Marcelo-Dias <34683289+Marcelo-Dias@users.noreply.github.com>
2021-06-22 23:32:24 -0300

    Adicionando colisões com  os Tiles

--- src/com/mdstudios/world/World.java
+++ src/com/mdstudios/world/World.java
@@ -11,8 +11,9 @@ import com.mdstudios.main.Game;
 
 public class World {
 	
-	private Tile[] tiles;
+	public static Tile[] tiles;
 	public static int WIDTH, HEIGHT;
+	public static final int TILE_SIZE = 16;
 	
 	public World(String path) {
 		try {
@@ -26,36 +27,55 @@ public class World {
 				for(int yy = 0; yy < map.getHeight(); yy++) {
 					int pixelAtual = pixels[xx + (yy * map.getWidth())];
 					
-					tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+					tiles[xx + (yy * WIDTH)] = new FloorTile(xx * TILE_SIZE, yy * TILE_SIZE, Tile.TILE_FLOOR);
 					
 					if(pixelAtual == 0xFF000000) {
 						//Floor
-						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * TILE_SIZE, yy * TILE_SIZE, Tile.TILE_FLOOR);
 					} else if(pixelAtual == 0xFFFFFFFF) {
 						//Wall/Parede
-						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_WALL);
+						tiles[xx + (yy * WIDTH)] = new WallTile(xx * TILE_SIZE, yy * TILE_SIZE, Tile.TILE_WALL);
 					} else if(pixelAtual == 0xFF0026FF) {
 						//Player
-						Game.player.setX(xx*16);
-						Game.player.setY(yy*16);
+						Game.player.setX(xx*TILE_SIZE);
+						Game.player.setY(yy*TILE_SIZE);
 					} else if(pixelAtual == 0xFFFF0000) {
 						//Enemy
-						Game.entities.add(new Enemy(xx*16, yy*16, 16, 16, Entity.ENEMY_EN));
+						Game.entities.add(new Enemy(xx*TILE_SIZE, yy*TILE_SIZE, TILE_SIZE, TILE_SIZE, Entity.ENEMY_EN));
 					} else if (pixelAtual == 0xFFFF6A00) {
 						//Weapon
-						Game.entities.add(new Weapon(xx*16, yy*16, 16, 16, Entity.WEAPON_EN));
+						Game.entities.add(new Weapon(xx*TILE_SIZE, yy*TILE_SIZE, TILE_SIZE, TILE_SIZE, Entity.WEAPON_EN));
 					} else if (pixelAtual == 0xFFFF7F7F) {
 						//Life Pack
-						Game.entities.add(new Lifepack(xx*16, yy*16, 16, 16, Entity.LIFEPACK_EN));
+						Game.entities.add(new Lifepack(xx*TILE_SIZE, yy*TILE_SIZE, TILE_SIZE, TILE_SIZE, Entity.LIFEPACK_EN));
 					} else if (pixelAtual == 0xFFFFD800) {
 						//Bullet
-						Game.entities.add(new Bullet(xx*16, yy*16, 16, 16, Entity.BULLET_EN));
+						Game.entities.add(new Bullet(xx*TILE_SIZE, yy*TILE_SIZE, TILE_SIZE, TILE_SIZE, Entity.BULLET_EN));
 					}
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static boolean isFree(int xnext, int ynext) {
+		int x1 = xnext / TILE_SIZE;
+		int y1 = ynext / TILE_SIZE;
+		
+		int x2 = (xnext + TILE_SIZE - 1) / TILE_SIZE;
+		int y2 = ynext / TILE_SIZE;
+		
+		int x3 = xnext / TILE_SIZE;
+		int y3 = (ynext + TILE_SIZE - 1) / TILE_SIZE;
+		
+		int x4 = (xnext + TILE_SIZE - 1) / TILE_SIZE;
+		int y4 = (ynext + TILE_SIZE - 1) / TILE_SIZE;
+		
+		return !((tiles[x1 + (y1 * World.WIDTH)] instanceof WallTile) ||
+				(tiles[x2 + (y2 * World.WIDTH)] instanceof WallTile) ||
+				(tiles[x3 + (y3 * World.WIDTH)] instanceof WallTile) ||
+				(tiles[x4 + (y4 * World.WIDTH)] instanceof WallTile));
 	}
 	
 	public void render(Graphics g) {
