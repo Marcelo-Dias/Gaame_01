9c6492b0308bec0be6fdec0f60f60415793cdcb3
Marcelo-Dias <34683289+Marcelo-Dias@users.noreply.github.com>
2021-07-31 23:08:45 -0300

    Adicionando arma e balas

--- src/com/mdstudios/entities/Player.java
+++ src/com/mdstudios/entities/Player.java
@@ -2,7 +2,9 @@ package com.mdstudios.entities;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import com.mdstudios.graficos.Spritesheet;
 import com.mdstudios.main.Game;
 import com.mdstudios.world.Camera;
 import com.mdstudios.world.World;
@@ -18,16 +20,22 @@ public class Player extends Entity{
 	private boolean moved = false;
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
+	private BufferedImage playerDamage;
+	private boolean hasWeapon = false;
+	public boolean shoot = false;
 	
 	public int ammo = 0;
+	public boolean isDamaged = false;
+	private int damageFrames = 0;
 	
-	public static double life = 100, maxLife = 100;
+	public double life = 100, maxLife = 100;
 
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 		
 		rightPlayer = new BufferedImage[4];
 		leftPlayer = new BufferedImage[4];
+		playerDamage = Game.spritesheet.getSprite(0, 16, 16, 16);
 		
 		for(int i = 0; i < 4; i++) {
 			rightPlayer[i] = Game.spritesheet.getSprite(32 + (i  * 16), 0, 16, 16);
@@ -68,12 +76,66 @@ public class Player extends Entity{
 		}
 		
 		this.checkCollisionLifePack();
+		this.checkCollisionWeapon();
 		this.checkCollisionAmmo();
+		
+		if(isDamaged) {
+			this.damageFrames++;
+			if(this.damageFrames == 8) {
+				this.damageFrames = 0;
+				isDamaged = false;
+			}
+		}
+		
+		if(shoot) {
+			shoot = false;
+			if(hasWeapon && ammo > 0) {
+				ammo--;
+				int dx = 0, px = 0, py = 8;
+				if(dir == right_dir) {
+					px = 18;
+					dx = 1;
+				}else {
+					px = -8;
+					dx = -1;
+				}
+				
+				BulletShoot bullet = new BulletShoot(this.getX() + px, this.getY() + py, 3, 3, null, dx, 0);
+				Game.bullets.add(bullet);
+				
+				System.out.println("Atirando!");
+			}
+		}
+		
+		if(life <= 0) {
+			Game.entities.clear();
+			Game.enemies.clear();
+			Game.entities = new ArrayList<Entity>();
+			Game.enemies = new ArrayList<Enemy>();
+			Game.spritesheet = new Spritesheet("/spritesheet.png");
+			Game.player = new Player(0, 0, 16, 16, Game.spritesheet.getSprite(32, 0, 16, 16));
+			Game.entities.add(Game.player);
+			Game.world = new World("/map.png");
+			return;
+		}
 		
 		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH / 2), 0, World.WIDTH * 16 - Game.WIDTH);
 		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT / 2), 0, World.HEIGHT * 16 - Game.HEIGHT);
 		
 	}
+
+	public void checkCollisionWeapon() {
+		for(int i = 0; i < Game.entities.size(); i++) {
+			Entity atual = Game.entities.get(i);
+			if(atual instanceof Weapon) {
+				if(Entity.isColidding(this, atual)) {
+					hasWeapon = true;
+					System.out.println("Pegou a arma");
+					Game.entities.remove(atual);
+				}
+			}
+		}
+	}	
 	
 	public void checkCollisionAmmo() {
 		for(int i = 0; i < Game.entities.size(); i++) {
@@ -103,10 +165,20 @@ public class Player extends Entity{
 	}
 	
 	public void render(Graphics g) {
-		if(dir == right_dir) {
-			g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
-		}else if(dir == left_dir) {
-			g.drawImage(leftPlayer[index], this.getX()- Camera.x, this.getY() - Camera.y, null);
+		if(!isDamaged) {
+			if(dir == right_dir) {
+				g.drawImage(rightPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				if(hasWeapon) {
+					g.drawImage(Entity.WEAPON_RIGHT, this.getX() + 9 - Camera.x, this.getY() - Camera.y, null);
+				}
+			}else if(dir == left_dir) {
+				g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+				if(hasWeapon) {
+					g.drawImage(Entity.WEAPON_LEFT, this.getX() - 9 - Camera.x, this.getY() - Camera.y, null);
+				}
+			}
+		}else {
+			g.drawImage(playerDamage, this.getX() - Camera.x, this.getY() - Camera.y, null);
 		}
 	}
 
--- src/com/mdstudios/graficos/UI.java
+++ src/com/mdstudios/graficos/UI.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
-import com.mdstudios.entities.Player;
+import com.mdstudios.main.Game;
 
 public class UI {
 	
@@ -12,10 +12,10 @@ public class UI {
 		g.setColor(Color.red);
 		g.fillRect(8,  4, 70, 8);
 		g.setColor(Color.green);
-		g.fillRect(8, 4, (int)((Player.life/Player.maxLife) * 70), 8);
+		g.fillRect(8, 4, (int)((Game.player.life/Game.player.maxLife) * 70), 8);
 		g.setColor(Color.white);
 		g.setFont(new Font("arial", Font.BOLD, 8));
-		g.drawString((int)Player.life+"/"+(int)Player.maxLife, 30, 11);
+		g.drawString((int)Game.player.life+"/"+(int)Game.player.maxLife, 30, 11);
 	}
 
 }
